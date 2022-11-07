5afaf92f5c6275d67908dbb24dbbedb2a0e8f198
Jym Bocala <jym.bocala@gmail.com>
2022-11-07 17:12:16 +1100

    add header component and styles

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src="../images/troll-face.png" id="troll-img" />
+      <h1>Meme Generator</h1>
+    </div>
+  );
+}
