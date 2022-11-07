ea35624fb3005f67adea9450864e8ff73bf58162
Jym Bocala <jym.bocala@gmail.com>
2022-11-07 17:13:55 +1100

    add alt to image

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="header">
-      <img src="../images/troll-face.png" id="troll-img" />
+      <img src="../images/troll-face.png" alt="troll face logo" id="troll-img" />
       <h1>Meme Generator</h1>
     </div>
   );
