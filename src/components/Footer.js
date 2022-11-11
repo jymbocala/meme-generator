055d191843fe3ebdf7e386cc8229e3608117f713
Jym Bocala <jym.bocala@gmail.com>
2022-11-11 23:28:44 +1100

    add foot component & styles

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Made with ❤️ by{" "}
+        <a
+          href="https://github.com/jymbocala"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer--link"
+        >
+          Jym
+        </a>
+      </p>
+    </footer>
+  );
+}
