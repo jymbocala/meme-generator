6ef2be153accd4cefcf2f6cd0223ae0d435809f2
Jym Bocala <jym.bocala@gmail.com>
2022-11-15 16:05:03 +1100

    add text to image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -23,27 +23,45 @@ export default function Meme() {
     });
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme(prevMeme => {
+      return {
+        ...prevMeme,
+        [name]: value
+      };
+    });
+  }
+
   return (
-    <main className="meme">
+    <main className="meme--main">
       <form className="meme--form">
         <input
           type="text"
           placeholder="Top Text"
           name="topText"
           className="form-input-top-text"
+          value={meme.topText}
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Bottom Text"
           name="bottomText"
           className="form-input-bottom-text"
+          value={meme.bottomText}
+          onChange={handleChange}
         />
         <button className="form-button" onClick={getMemeImage}>
           Get an new meme image 🖼️
         </button>
       </form>
 
-      <img src={meme.url} alt="render of a meme" className="meme--image" />
+      <div className="meme">
+        <img src={meme.url} alt="render of a meme" className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
